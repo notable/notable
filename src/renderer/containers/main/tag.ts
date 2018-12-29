3a87eafb53f1d39835992573c346fb9ec31c3818
Fabio Spampinato <spampinabio@gmail.com>
2018-12-29 02:06:49 +0100

    Tag collapsing/expanding: ensuring notebooks and templates don’t get duplicated
    Closes #11

--- src/renderer/containers/main/tag.ts
+++ src/renderer/containers/main/tag.ts
@@ -89,8 +89,14 @@ class Tag extends Container<TagState, MainCTX> {
 
         parent.tags[obj.name] = obj;
 
-        tags[TAGS].tags[parentPartRoot] = tags[parentPartRoot]; // It's import to update the TAGS tag too
-        tags[TAGS] = _.clone ( tags[TAGS] );
+        const isSpecial = ( tag === NOTEBOOKS ) || ( tag === TEMPLATES ) || tag.startsWith ( `${NOTEBOOKS}${Tags.SEPARATOR}` ) || tag.startsWith ( `${TEMPLATES}${Tags.SEPARATOR}` );
+
+        if ( !isSpecial ) { // It's important to update the TAGS tag too
+
+          tags[TAGS].tags[parentPartRoot] = tags[parentPartRoot];
+          tags[TAGS] = _.clone ( tags[TAGS] );
+
+        }
 
       }
 
