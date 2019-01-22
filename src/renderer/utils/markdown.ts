1d776cb67fc0ad474f2cfbfd4e4afac872a9b693
Fabio Spampinato <spampinabio@gmail.com>
2019-01-22 19:04:27 +0100

    Optimized Markdown rendering when no Markdown features are used

--- src/renderer/utils/markdown.ts
+++ src/renderer/utils/markdown.ts
@@ -27,6 +27,8 @@ const Markdown = {
 
   converter: undefined,
 
+  markdownRe: /_|\*|~|`|<|:|^>|^#|\]|---|===|\d\.|[*+-][ \t]+\[|\n\n/m,
+
   extensions: {
 
     checkbox () {
@@ -243,7 +245,15 @@ const Markdown = {
 
   },
 
+  is: ( str: string ): boolean => { // Checks if `str` _could_ be using some Markdown features, it doesn't tell reliably when it actually is, only when it isn't. Useful for skipping unnecessary renderings
+
+    return Markdown.markdownRe.test ( str );
+
+  },
+
   render: _.memoize ( ( str: string ): string => {
+
+    if ( !Markdown.is ( str ) ) return `<p>${str}</p>`;
 
     return Markdown.getConverter ().makeHtml ( str );
 
