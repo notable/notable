6ba453de103ff1573a07a8bd8c201b8c1b73d6ec
Fabio Spampinato <spampinabio@gmail.com>
2019-01-22 17:58:53 +0100

    Experimenting usage of @atom/watcher

--- src/renderer/utils/watcher.ts
+++ src/renderer/utils/watcher.ts
@@ -1,95 +1,15 @@
 
 /* IMPORT */
 
-import * as _ from 'lodash';
-
-/* IMPORT LAZY */
-
-const laxy = require ( 'laxy' ),
-      chokidar = laxy ( () => require ( 'chokidar' ) )();
+import * as watch from '@atom/watcher';
 
 /* WATCHER */
 
-//TODO: Maybe switch to `https://github.com/Axosoft/nsfw`
+async function watcher ( root: string, options = {}, callbacks = {} ) {
 
-function watcher ( root: string, options = {}, callbacks = {} ) {
-
-  /* VARIABLES */
-
-  let filesStats = {}, // filePath => fs.Stats
-      renameSignalers = {}; // ino => Function
-
-  /* HELPERS */
-
-  function emit ( event, args ) {
-
-    if ( !callbacks[event] ) return;
-
-    callbacks[event].apply ( undefined, args );
-
-  }
-
-  /* HANDLERS */
-
-  async function add ( filePath, stats ) {
-
-    stats = stats || {}; // Just to be safe
-
-    filesStats[filePath] = stats;
-
-    const renameSignaler = renameSignalers[stats.ino];
-
-    if ( renameSignaler ) {
-
-      const prevPath = renameSignaler ();
-
-      emit ( 'rename', [prevPath, filePath, stats] );
-
-    } else {
-
-      emit ( 'add', [filePath, stats] );
-
-    }
-
-  }
-
-  function change () {
-
-    emit ( 'change', arguments );
-
-  }
-
-  function unlink ( filePath ) {
-
-    const args = arguments; //TSC
-
-    const stats = filesStats[filePath] || {}; // Just to be safe
-
-    const timeoutId = setTimeout ( () => { // Deleted
-
-      delete renameSignalers[stats.ino];
-
-      emit ( 'unlink', args );
-
-    }, 150 );
-
-    renameSignalers[stats.ino] = () => { // Renamed
-
-      clearTimeout ( timeoutId );
-
-      delete renameSignalers[stats.ino];
-
-      return filePath;
-
-    };
-
-  }
-
-  /* CHOKIDAR */
-
-  const chokidarOptions = _.merge ( {}, options, { ignoreInitial: false } );
-
-  return chokidar.watch ( root, chokidarOptions ).on ( 'add', add ).on ( 'change', change ).on ( 'unlink', unlink );
+  return await watch.watchPath ( root, options, events => {
+    alert ( `Received batch of ${events.length} events` );
+  });
 
 }
 
