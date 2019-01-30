5a7d954661b2735554a9d4956f65fd7aced3f1dd
Steven Chester <steven.chester@industryweapon.com>
2019-01-30 14:46:51 -0500

    adds a 'clear search' icon, solves issue #360

--- src/renderer/components/main/middlebar/toolbar_search.tsx
+++ src/renderer/components/main/middlebar/toolbar_search.tsx
@@ -9,22 +9,39 @@ import Main from '@renderer/containers/main';
 /* SEARCH */
 
 class Search extends React.Component<any, undefined> {
-
+ 
   ref = React.createRef () as any; //TSC
 
   onChange = _.debounce ( () => {
-
+    
     this.props.setQuery ( this.ref.current.value );
 
   }, 50 );
 
+  constructor ( props ) {
+
+    super ( props );
+
+    this.onClearSearch = this.onClearSearch.bind ( this );
+
+  }
+  
+  onClearSearch() {
+
+    this.props.clearQuery()
+
+    this.ref.current.value = ""
+  }
+
   render () {
+    const isSearching = this.props.query && this.props.query.length > 0
 
     return (
       <div className="multiple joined no-separators grow search">
         <input ref={this.ref} autoFocus type="search" className="bordered grow small" placeholder="Search notes..." defaultValue={this.props.query} onChange={this.onChange} />
         <div className="label bordered xsmall" title="Search">
-          <i className="icon">magnify</i>
+          {isSearching && <i className="icon" onClick={this.onClearSearch}>close</i>}
+          {!isSearching && <i className="icon">magnify</i>}
         </div>
       </div>
     );
@@ -37,9 +54,10 @@ class Search extends React.Component<any, undefined> {
 
 export default connect ({
   container: Main,
-  shouldComponentUpdate: false,
+  shouldComponentUpdate: true,
   selector: ({ container }) => ({
     query: container.search.getQuery (),
-    setQuery: container.search.setQuery
+    setQuery: container.search.setQuery,
+    clearQuery: container.search.clear,
   })
 })( Search );
