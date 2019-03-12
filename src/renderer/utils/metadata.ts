7e8fe8f43d517c54aa5df0ad043fca7aaf8cd26a
Fabio Spampinato <spampinabio@gmail.com>
2019-03-12 02:07:59 +0100

    Improved start/end newline normalization

--- src/renderer/utils/metadata.ts
+++ src/renderer/utils/metadata.ts
@@ -5,6 +5,24 @@ import * as _ from 'lodash';
 import * as matter from 'gray-matter';
 import * as yaml from 'js-yaml';
 
+/* GUTTER */
+
+const Gutter = { // Add/remove an empty line at the start/end //TODO: Maybe find a better name
+
+  add ( str: string ): string {
+
+    return `\n${str}\n`;
+
+  },
+
+  remove ( str: string ): string {
+
+    return str.replace ( /^\n/, '' ).replace ( /\n$/, '' );
+
+  }
+
+};
+
 /* PARSER */
 
 const Parser = {
@@ -57,7 +75,7 @@ const Metadata = {
 
   set ( content: string, metadata: object ): string {
 
-    content = Metadata.remove ( content );
+    content = Gutter.add ( matter ( content, Metadata.options ).content );
 
     if ( !_.isEmpty ( metadata ) ) {
 
@@ -71,7 +89,7 @@ const Metadata = {
 
   remove ( content: string ): string {
 
-    return matter ( content, Metadata.options ).content;
+    return Gutter.remove ( matter ( content, Metadata.options ).content );
 
   }
 
