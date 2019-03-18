6c219bef3f6ab89f185729b452ff7da5fe620737
Mikael Mello <git+github@mikaelmello.com>
2019-03-18 01:29:49 -0300

    Enable scrolling past last line

--- src/renderer/utils/monaco.ts
+++ src/renderer/utils/monaco.ts
@@ -50,7 +50,7 @@ const Monaco = {
       verticalScrollbarSize: 12
     },
     scrollBeyondLastColumn: 0,
-    scrollBeyondLastLine: false,
+    scrollBeyondLastLine: true,
     snippetSuggestions: 'none',
     wordWrap: Settings.get ( 'monaco.editorOptions.wordWrap' ),
     wordWrapColumn: 1000000,
