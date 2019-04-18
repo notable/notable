f0117b205c7070fb7fad17df19da5b0d47ac7040
afpr <armagedoom@gmail.com>
2019-04-18 20:34:01 +0200

    copy button per tag in tags popover

--- src/renderer/components/main/popovers/tag.tsx
+++ src/renderer/components/main/popovers/tag.tsx
@@ -5,15 +5,16 @@ import * as _ from 'lodash';
 import * as React from 'react';
 import {connect} from 'overstated';
 import Main from '@renderer/containers/main';
+import {clipboard} from 'electron';
 
 /* TAG */
-
 const Tag = ({ tag, set, removeTag }) => {
 
   if ( !tag ) return null;
 
   return (
     <div className="tag button list-item" data-tag={tag} onClick={() => set ( tag )}>
+      <i className="icon xxsmall copy actionable on-hover" onClick={e => { e.stopPropagation (); clipboard.writeText (  tag ); }}>content_copy</i>
       <span className="title small">{tag}</span>
       <i className="icon xxsmall actionable on-hover" onClick={e => { e.stopPropagation (); removeTag ( undefined, tag ); }}>close</i>
     </div>
