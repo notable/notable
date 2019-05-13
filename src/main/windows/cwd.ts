381fa78437d6ca9dbf3f7c126fd67c8e61fd7be0
Fabio Spampinato <spampinabio@gmail.com>
2019-05-14 01:46:07 +0200

    Auto-hiding the menu in the “Select Data Directory” window

--- src/main/windows/cwd.ts
+++ src/main/windows/cwd.ts
@@ -11,7 +11,7 @@ class CWD extends Route {
 
   /* CONSTRUCTOR */
 
-  constructor ( name = 'cwd', options: BrowserWindowConstructorOptions = { resizable: false, minWidth: 560, minHeight: 485 }, stateOptions: windowStateKeeper.Options = { defaultWidth: 560, defaultHeight: 485 } ) {
+  constructor ( name = 'cwd', options: BrowserWindowConstructorOptions = { autoHideMenuBar: true, resizable: false, minWidth: 560, minHeight: 485 }, stateOptions: windowStateKeeper.Options = { defaultWidth: 560, defaultHeight: 485 } ) {
 
     super ( name, options, stateOptions );
 
