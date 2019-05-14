d0adc71901bf3284e866e370febba7643a22ece4
Fabio Spampinato <spampinabio@gmail.com>
2019-05-14 17:29:13 +0200

    Updater: fixed link to latest windows release

--- scripts/build/hook_after_build.js
+++ scripts/build/hook_after_build.js
@@ -1,13 +1,15 @@
 
 /* IMPORT */
 
-const fixLatestLinux = require ( './fix_latest_linux' );
+const fixLatestLinux = require ( './fix_latest_linux' ),
+      fixLatestWin = require ( './fix_latest_win' );
 
 /* AFTER BUILD */
 
 function afterBuild () {
 
   fixLatestLinux ();
+  fixLatestWin ();
 
   return [];
 
