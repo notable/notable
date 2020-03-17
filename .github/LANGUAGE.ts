b8d7c0d15e2c11dcd0627470745010adbe6db713
Fabio Spampinato <spampinabio@gmail.com>
2020-03-17 17:36:56 +0000

    Create LANGUAGE.ts

---
+++ .github/LANGUAGE.ts
@@ -0,0 +1,3 @@
+
+// This is a placeholder file, its purpose is to signal GitHub what language the code available in this repository is written in, as GitHub isn't able to infer this by itself anymore since the old open-source code has been pushed down git's history
+// More info: https://github.com/notable/notable/blob/master/SOURCE_CODE.md
